8d565b2bf9d8e6fd86691ff60c508aeb2e2cbb2d
binch95 <qlscm95@gmail.com>
2024-07-11 15:05:18 +0900

    카카오 선ë신고결과 시간줄이기

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -6,24 +6,43 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Solution.solution(new String[]{"con", "ryan"}, new String[]{"ryan con", "ryan con", "ryan con", "ryan con"}, 2));
+        System.out.println(Solution.solution(new String[]{"a", "b", "c", "d"}, new String[]{"a b", "a b", "c a", "d a", "a b", "c d", "a d", "b c", "b a", "d a"}, 2));
     }
 }
 
 class Solution {
     public static Collection<Integer> solution(String[] id_list, String[] report, int k) {
         Map<String, Integer> map1 = new LinkedHashMap<>();
-        Set<String> set1 = new LinkedHashSet<>(List.of(report));
-        for (String re : id_list) {
-            map1.put(re, 0);
-            ArrayList<String> xx = new ArrayList<>(Arrays.asList(report).stream().distinct().filter(e -> e.split(" ")[0].equals(re)).collect(Collectors.toList()));
-            for (int i = 0; i < xx.size(); i++) {
-                String[] q = xx.get(i).split(" ");
-                if (Arrays.stream(report).distinct().filter(e -> e.split(" ")[1].equals(q[1])).count() >= k) {
-                    map1.put(q[0], map1.get(q[0]) + 1);
-                }
-            }
+        Map<String, Integer> map2 = new LinkedHashMap<>();
+        for (String id : id_list){
+            map2.put(id, 0);
         }
-        return map1.values();
+        for (String re : Arrays.stream(report).distinct().collect(Collectors.toList())) {
+            map1.put(re.split(" ")[1], map1.getOrDefault(re.split(" ")[1],0) + 1);
+        }
+
+            for (String re : Arrays.stream(report).distinct().collect(Collectors.toList())) {
+                if(map1.get(re.split(" ")[1]) >= k){
+                map2.put(re.split(" ")[0], map2.get(re.split(" ")[0]) + 1);}
+            }
+        return map2.values();
     }
 }
+
+
+//class Solution {
+//    public static Collection<Integer> solution(String[] id_list, String[] report, int k) {
+//        Map<String, Integer> map1 = new LinkedHashMap<>();
+//        for (String re : id_list) {
+//            map1.put(re, 0);
+//            ArrayList<String> xx = new ArrayList<>(Arrays.asList(report).stream().distinct().filter(e -> e.split(" ")[0].equals(re)).collect(Collectors.toList()));
+//            for (int i = 0; i < xx.size(); i++) {
+//                String[] q = xx.get(i).split(" ");
+//                if (Arrays.stream(report).distinct().filter(e -> e.split(" ")[1].equals(q[1])).count() >= k) {
+//                    map1.put(q[0], map1.get(q[0]) + 1);
+//                }
+//            }
+//        }
+//        return map1.values();
+//    }
+//}
