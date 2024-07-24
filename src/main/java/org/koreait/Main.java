0b1a7cd6601a90838da50f92da1b46c1095ccb85
binch95 <qlscm95@gmail.com>
2024-07-24 11:22:39 +0900

    프로그래머스 ì연속된 부분 수열의 합

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -6,43 +6,42 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Solution.solution(new String[]{"a", "b", "c", "d"}, new String[]{"a b", "a b", "c a", "d a", "a b", "c d", "a d", "b c", "b a", "d a"}, 2));
+        System.out.println(Solution.solution(new int[]{1, 2, 3, 4, 5}, 7));
     }
 }
 
 class Solution {
-    public static Collection<Integer> solution(String[] id_list, String[] report, int k) {
-        Map<String, Integer> map1 = new LinkedHashMap<>();
-        Map<String, Integer> map2 = new LinkedHashMap<>();
-        for (String id : id_list){
-            map2.put(id, 0);
-        }
-        for (String re : Arrays.stream(report).distinct().collect(Collectors.toList())) {
-            map1.put(re.split(" ")[1], map1.getOrDefault(re.split(" ")[1],0) + 1);
-        }
-
-            for (String re : Arrays.stream(report).distinct().collect(Collectors.toList())) {
-                if(map1.get(re.split(" ")[1]) >= k){
-                map2.put(re.split(" ")[0], map2.get(re.split(" ")[0]) + 1);}
+    public static int[] solution(int[] sequence, long k) {
+        int min = Integer.MAX_VALUE;
+        int start = 0;
+        int end   = 0;
+        long cur  = 0;
+        int answer_st = -1;
+        int answer_en = -1;
+        for(int i = 0; i < sequence.length; i++){
+            end++;
+            cur += sequence[i];
+            if(cur < k) continue;
+            if(cur == k){
+                if(min > end - start + 1){
+                    min = end - start + 1;
+                    answer_st = start;
+                    answer_en = end - 1;
+                }
             }
-        return map2.values();
+            else{
+                while(cur > k){
+                    cur -= sequence[start++];
+                }
+                if(cur == k){
+                    if(min > end - start + 1){
+                        min = end - start + 1;
+                        answer_st = start;
+                        answer_en = end - 1;
+                    }
+                }
+            }
+        }
+        return new int []{answer_st, answer_en};
     }
 }
-
-
-//class Solution {
-//    public static Collection<Integer> solution(String[] id_list, String[] report, int k) {
-//        Map<String, Integer> map1 = new LinkedHashMap<>();
-//        for (String re : id_list) {
-//            map1.put(re, 0);
-//            ArrayList<String> xx = new ArrayList<>(Arrays.asList(report).stream().distinct().filter(e -> e.split(" ")[0].equals(re)).collect(Collectors.toList()));
-//            for (int i = 0; i < xx.size(); i++) {
-//                String[] q = xx.get(i).split(" ");
-//                if (Arrays.stream(report).distinct().filter(e -> e.split(" ")[1].equals(q[1])).count() >= k) {
-//                    map1.put(q[0], map1.get(q[0]) + 1);
-//                }
-//            }
-//        }
-//        return map1.values();
-//    }
-//}
